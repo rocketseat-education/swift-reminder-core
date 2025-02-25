ba5d5016bbd931de35e38e4a6cf720f18e91da53
arthur <arthur.rios007@gmail.com>
2025-02-25 13:47:55 -0300

    Cocoapods e Modulos, CoreApp - Estrutura do Onboarding (UIView)

--- CoreFramework/CoreFramework/Sources/Onboarding/OnboardingView.swift
+++ CoreFramework/CoreFramework/Sources/Onboarding/OnboardingView.swift
@@ -14,8 +14,94 @@ public class OnboardingView: UIView {
     
     private let backgroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black.withAlphaComponent(0.4)
+        view.backgroundColor = Colors.gray100.withAlphaComponent(0.4)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.gray300
+        label.font = Typography.heading
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Próximo", for: .normal)
+        button.titleLabel?.font = Typography.subHeading
+        button.setTitleColor(Colors.primaryRedBase, for: .normal)
+        button.addTarget(self, action: #selector(didTapNextStep), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    public init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(backgroundView)
+        addSubview(messageLabel)
+        addSubview(nextButton)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metrics.medium),
+            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Metrics.medium),
+            
+            nextButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: Metrics.medium),
+            nextButton.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+    
+    public func presentOnboarding(on view: UIView, with steps: [String]) {
+        self.steps = steps
+        self.currentStep = 0
+        //criar uma func para iterar o step +1
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func updateStep() {
+        messageLabel.text = steps[currentStep]
+    }
+    
+    @objc
+    private func didTapNextStep() {
+        currentStep += 1
+        if currentStep < steps.count {
+            updateStep()
+        } else {
+            dismiss()
+        }
+    }
+    
+    private func dismiss() {
+        removeFromSuperview()
+    }
 }
