6971baf81687be26ef654045fcd78e977dacdb4d
arthur <arthur.rios007@gmail.com>
2025-02-25 16:08:27 -0300

    Cocoapods e Modulos, CoreApp - Introdução ao Example

---
+++ ExampleCore/ExampleCore/MenuView.swift
@@ -0,0 +1,14 @@
+//
+//  MenuView.swift
+//  ExampleCore
+//
+//  Created by Arthur Rios on 25/02/25.
+//
+
+import Foundation
+import UIKit
+import CoreFramework
+
+class MenuView: UIView {
+    
+}
---
+++ ExampleCore/ExampleCore/MenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ExampleCore
+//
+//  Created by Arthur Rios on 25/02/25.
+//
+
+import UIKit
+//import CoreFramework
+
+class MenuViewController: UIViewController {
+    private let contentView: MenuView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(contentView)
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
